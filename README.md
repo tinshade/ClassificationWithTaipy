560dcbee6d2472079aee5b9f3edb4568a25cf260
Abhishek Iyengar <tinshade98@gmail.com>
2023-11-27 01:54:13 +0530

    Adding SS and other stuff

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ClassificationWithTaipy
-A Taipy Web GUI based classification app that tells you what the uploaded image with a confidence metric
+A Taipy Web GUI based classification app that tells you what the uploaded image with a confidence metric.
+Learn how to do that here: https://docs.taipy.io/en/release-3.0/knowledge_base/tips/taipy_cloud_deploy/
+Check it out here: https://attempt-1.taipy.cloud/
+
+![Screenshot Of The GUI](./assets/SS1.png)
+
---
+++ deploy/main.py
@@ -0,0 +1,80 @@
+from taipy.gui import Gui
+from tensorflow.keras import models
+from PIL import Image
+import numpy as np
+
+model = models.load_model("baseline_mariya.keras")
+
+
+class_names = {
+    0: "airplane",
+    1: "automobile",
+    2: "bird",
+    3: "cat",
+    4: "deer",
+    5: "dog",
+    6: "frog",
+    7: "horse",
+    8: "ship",
+    9: "truck",
+}
+
+
+def predict_image(model, path_to_image):
+    img = Image.open(path_to_image)
+    img = img.convert("RGB").resize((32, 32))
+
+    # Normalizing image
+    data = np.asarray(img)
+    print("Before: ", data[0][0])  # Printing color of very first pixel
+    data = data / 255
+
+    # Comparing stuff to see if we broke something
+    print("After: ", data[0][0])  # Printing color of very first pixel
+
+    # Tricking model into thinking it is looking at an array of sample images and not a single image
+    probability = model.predict(np.array([data])[:1])
+    print(probability)
+    probes = probability.max()
+    prediction = class_names[np.argmax(probability)]
+    return (probes, prediction)
+
+
+image_path = "placeholder_image.png"
+prediction, prob, content = "", "", ""
+
+
+image_control_component = """
+<|text-center|
+<|{"logo.png"}|image|width=25vw|>
+
+<|{content}|file_selector|extensions=.png|>
+
+Select an image!
+
+
+<|{prediction}|>
+
+
+
+<|{image_path}|image|>
+
+<|{prob}|indicator|value={prob}|min=0|max=100|width=25vw|>
+>
+"""
+
+
+index = image_control_component
+
+
+def on_change(state, variable_name, variable_value):
+    if variable_name == "content":
+        state.image_path = variable_value
+        probes, prediction = predict_image(model, variable_value)
+        state.prob = round(probes * 100)  # Converting decimal to percentage
+        state.prediction = f"This is a : {prediction}"
+
+
+app = Gui(page=index)
+if __name__ == "__main__":
+    app.run(use_reloader=True)
